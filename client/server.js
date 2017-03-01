a533fe838381863ef44bd08fc2bd26486f76c6d1
Andrew Makenzi <andrew.makenzi@andela.com>
2017-03-01 21:16:12 +0300

    chore(webpack): configure frontend tooling
    - setup angular to transpile ts
    - setup angular to load scss
    - configure angular to load static files

---
+++ client/server.js
@@ -0,0 +1,39 @@
+const StaticServer = require('static-server');
+require('dotenv').config();
+
+const server = new StaticServer({
+  rootPath: process.env.rootPath,            // required, the root of the server file tree 
+  name: process.env.name,   // optional, will set "X-Powered-by" HTTP header 
+  port: process.env.port,              // optional, defaults to a random port 
+  host: process.env.host,       // optional, defaults to any interface 
+  cors: '*',                 // optional, defaults to undefined 
+  followSymlink: true,      // optional, defaults to a 404 error 
+  templates: {
+    index: 'foo.html',      // optional, defaults to 'index.html' 
+    notFound: '404.html'    // optional, defaults to undefined 
+  }
+});
+ 
+server.start(function () {
+  console.log('Server listening to', server.port);
+});
+ 
+server.on('request', function (req, res) {
+  // req.path is the URL resource (file name) from server.rootPath 
+  // req.elapsedTime returns a string of the request's elapsed time 
+});
+ 
+server.on('symbolicLink', function (link, file) {
+  // link is the source of the reference 
+  // file is the link reference 
+  console.log('File', link, 'is a link to', file);
+});
+ 
+server.on('response', function (req, res, err, file, stat) {
+  // res.status is the response status sent to the client 
+  // res.headers are the headers sent 
+  // err is any error message thrown 
+  // file the file being served (may be null) 
+  // stat the stat of the file being served (is null if file is null) 
+
+});
