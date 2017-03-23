85d356741c733547e23d422502adbf90aa5610ea
andela-jmwalo <judith.achieng@andela.com>
2017-03-23 14:48:06 +0300

    feat(filters): update variable names

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -9,16 +9,16 @@ import { AccordionModule } from 'ngx-accordion';
 export class DashboardComponent implements OnInit {
 
   closed = true ;
-  icon_names = ['add_box', 'remove_circle'];
-  icon1_name= 'add_box';
-  icon2_name= 'add_box';
+  iconNames = ['add_box', 'remove_circle'];
+  icon1Name= 'add_box';
+  icon2Name= 'add_box';
 
   constructor() { }
 
   toggleIcon(itemClickedIndex: Number) {
     this.closed = !this.closed;
     const index = this.closed === true ? 0 : 1;
-    itemClickedIndex === 1 ? this.icon1_name = this.icon_names[index] : this.icon2_name = this.icon_names[index];
+    itemClickedIndex === 1 ? this.icon1Name = this.iconNames[index] : this.icon2Name = this.iconNames[index];
   }
 
   ngOnInit() {
