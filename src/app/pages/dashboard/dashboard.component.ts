180461801efde0551fe5c177eb56b747d6de6e5f
andela-jmwalo <judith.achieng@andela.com>
2017-03-23 10:59:56 +0300

    feat(filters): set icon change when accordion group is opened

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -8,7 +8,18 @@ import { AccordionModule } from 'ngx-accordion';
 })
 export class DashboardComponent implements OnInit {
 
+  closed = true ;
+  icon_names = ['add_box', 'remove_circle'];
+  icon1_name= 'add_box';
+  icon2_name= 'add_box';
+
   constructor() { }
+
+  dothis(itemClickedIndex: Number) {
+    this.closed = !this.closed;
+    const index = this.closed === true ? 0 : 1;
+    itemClickedIndex === 1 ? this.icon1_name = this.icon_names[index] : this.icon2_name = this.icon_names[index];
+  }
 
   ngOnInit() {
   }
