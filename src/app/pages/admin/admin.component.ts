5c3837ef55167d5098ea3acc75cb57d58a15477f
Jorg Are <jorg.are@andela.com>
2017-04-22 17:57:28 +0100

    feat(request-filter): filter request list base on primary skills (#46)
    * feat(request-filter): filter request list base on primary skills
    [Finishes #143403933]
    
    * feat(comment): add comments to methods
    
    * feat(nitpick): pluralize request in getRequests method comment

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '@angular/material';
 import { RequestService } from '../../services/request.service';
-
+import { FilterService } from '../../services/filter.service';
 
 @Component({
   templateUrl: './admin.component.html',
@@ -13,28 +13,50 @@ export class AdminComponent implements OnInit {
   private requestedBy: string = 'Abolaji Femi';
   private limit: number = 10;
   private loading: boolean = false;
+  private filteredSkills: any[] = [];
 
-  constructor (private requests: RequestService ) {}
+  constructor (
+    private requests: RequestService,
+    private filterService: FilterService) {}
 
   ngOnInit() {
     this.getRequests(this.limit);
+    this.watchFilters();
   }
 
-  getRequests(limit: number):void {
+  /**
+   * gets all requests from request service
+   *
+   * @param {Number} limit - number of requests to return
+   * @return {Void}
+   */
+  getRequests(limit: number): void {
     this.loading = true;
     this.requests.getRequests(limit)
       .subscribe(requests => {
         this.loading = false;
-        this.extractRequest(requests)
-      })
+        this.extractRequest(requests);
+      });
   }
 
-  extractRequest(requestsArray: Array<Object>): any {
+  /**
+   * pushes each request into a request array
+   *
+   * @param {Array} requestsArray - Array of requests
+   * @return {Void}
+   */
+  extractRequest(requestsArray: Array<Object>): void {
     requestsArray.forEach(request => {
       this.allRequests.push(request);
     });
   }
 
+  /**
+   * returns a css class for chips based on request status
+   *
+   * @param {String} status - request status
+   * @return {String} statusClass - css class
+   */
   getStatusClass(status: string): string {
     let statusClass: string = '';
 
@@ -47,4 +69,13 @@ export class AdminComponent implements OnInit {
     return statusClass;
   }
 
+  /**
+   *  watches for any changes in the checkedSkills arrays in the filters service
+   *
+   * @return {Void}
+   */
+  watchFilters(): void {
+    this.filterService.getCheckedSkills()
+      .subscribe(skills => this.filteredSkills = skills);
+  }
 }
