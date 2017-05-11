5517d7cdb1ce20214085a368fc6e38ae71038aee
Allan <allan.mwesigwa@andela.com>
2017-05-11 20:30:59 +0300

    feat(requests-form): Change the time input fields in requests form (#81)
    * feat(requests-form): change the time input fields.
    
    - the start and end time inputs for selecting period of pairing time change from html time fields to a dropdown.
    
    * style(time-widget): align start, end and timezone controls all on one line
    
    * fix(skills filter): fix skills filter (#74)
    
    * fix(skills filter): fix skills filter
    
    * fix(skills filter): create and use helper class to display skills
    
    * fix(skills filter): refactor skills pipe to use helper class
    
    * fix(skills filter): update helper class
    
    * fix(skills filter): implement changes from PR review
    [Finishes #145005647]
    
    * feat(edit-request): user's should be able to view an edit mentorship request form (#77)
    
    * feat(edit-request): implement edit mentorship request
    
    * refactor(edit-mentorship-request): refactor edit request to use data from request detailed page
    
    [Finishes #140653145]
    
    * refactor(filters): refactor filters service to cache server results (#50)
    
    * feature(status-filter): get all results that matches the selected status
    
    * feature(fillter): add method that keeps track of clicked filter
    
    add method that keeps track of clicked filter, makes call to the server and returns all matching result.
    
    [Finishes #143404367]
    
    * feature(filter-caching): add method that checks for duplicates and populates the result container with new result.
    
    * refactor(admin-component): refactor  extractRequest method to make it more efficient
    
    * refactor(admin-component): rename services injected into compoent and refactor extractRequest logic
    
    * style(merge-conflict): fix merge conflict while pulling from develop and edit jsdoc for methods
    
    * refactor(skills): fix skills display on mentor's dashboard
    
    * style(jsdoc): add more documentation to explain code and space them
    
    * refactor(requests-components): refactor switch case in getRequestsByFilter method
    
    * style(spinner): add md-spinner to admin template
    
    * refactor(filters): add subscribed filter to ngOnDestroy method
    
    * style(docs): edit docs in method
    
    * refactor: cascade switch statement
    
    [Finishes #143404367]
    
    * feat(mentee-requests): users should only see their own requests on the mentee page (#78)
    
    * bg(skills): fix skills endpoint
    
    * fix(mentee-bug): update the get mentee requests endpoint
    
    * feat(request-interested): add user feedback when user indicates interest in request (#79)
    
    * feat(admin): only admin should have access to the see all requests page (#80)
    
    * feat(admin): add admin-guard service
    
    * refactor(admin): change and access the roles claim instead of the permissions claim from jwt token
    
    * refactor(admin): fix indentation
    
    * feat(request-interested): add user feedback when user indicates interest in request (#79)
    [Delivers #145003135]
    
    * feat(requests-form): change the time input fields.
    
    - the start and end time inputs for selecting period of pairing time change from html time fields to a dropdown.
    
    * style(time-widget): align start, end and timezone controls all on one line
    
    [Delivers #145020673]

--- src/app/pages/requests/requests.component.ts
+++ src/app/pages/requests/requests.component.ts
@@ -40,140 +40,140 @@ export class RequestsComponent implements OnInit {
     this.snackBarConfig = { duration: 3000 };
   }
 
-    ngOnInit() {
-      this.form = new FormGroup({
-        requiredSkills: new FormControl('', [Validators.required]),
-        otherSkills: new FormControl(''),
-        duration: new FormControl('', [Validators.required]),
-        timeControlStart: new FormControl('', [Validators.required]),
-        timeControlEnd: new FormControl('', [Validators.required]),
-        timeZone: new FormControl('', [Validators.required]),
-        selectedDays: new FormArray(this.daysOfAvailability
-          .map(() => new FormControl(false)), this.validateSelectedDays),
-        description: new FormControl('', [Validators.required]),
-        title: new FormControl('', [Validators.required])
-      });
-      this.fetchSkills();
-      this.initMonths();
-      this.setDays();
-    }
+  ngOnInit() {
+    this.form = new FormGroup({
+      requiredSkills: new FormControl('', [Validators.required]),
+      otherSkills: new FormControl(''),
+      duration: new FormControl('', [Validators.required]),
+      timeControlStart: new FormControl('', [Validators.required]),
+      timeControlEnd: new FormControl('', [Validators.required]),
+      timeZone: new FormControl('', [Validators.required]),
+      selectedDays: new FormArray(this.daysOfAvailability
+        .map(() => new FormControl(false)), this.validateSelectedDays),
+      description: new FormControl('', [Validators.required]),
+      title: new FormControl('', [Validators.required])
+    });
+    this.fetchSkills();
+    this.initMonths();
+    this.setDays();
+  }
 
-    /**
-     * Maps the length of mentorship array to be used in the select input field
-     * for duration of mentorship
-     * @return {Object} number of months
-     */
-    initMonths() {
-      this.numMonths = this.lengthOfMentorship.map((item) => ({
-        'label' : item,
-        'value' : item
-      }));
-    }
+  /**
+   * Maps the length of mentorship array to be used in the select input field
+   * for duration of mentorship
+   * @return {Object} number of months
+   */
+  initMonths() {
+    this.numMonths = this.lengthOfMentorship.map((item) => ({
+      'label' : item,
+      'value' : item
+    }));
+  }
 
-    /**
-     * Maps the days of the week array to be used in the days available
-     * select input field
-     * @return {Object} days of the week
-     */
-    setDays() {
-      this.days = this.daysOfAvailability.map((item) => ({
-        'label' : item,
-        'value' : item
-      }));
-    }
+  /**
+   * Maps the days of the week array to be used in the days available
+   * select input field
+   * @return {Object} days of the week
+   */
+  setDays() {
+    this.days = this.daysOfAvailability.map((item) => ({
+      'label' : item,
+      'value' : item
+    }));
+  }
 
-    /**
-     * Gets the list of skills from the server
-     * @return {void}
-     */
-    fetchSkills() {
-      this.skillService
-        .getSkills()
-        .subscribe(res => this.setSkills(res));
-    }
+  /**
+   * Gets the list of skills from the server
+   * @return {void}
+   */
+  fetchSkills() {
+    this.skillService
+      .getSkills()
+      .subscribe(res => this.setSkills(res));
+  }
 
-    /**
-     * Maps the returned skills list to be used in the select skills input field
-     * select input field
-     * @param {Array} skills Array of object to be mapped
-     * @return {Object} list of skills
-     */
-    setSkills(skills) {
-      this.skills = skills.map(skill => ({
-        'label' : skill.name,
-        'value' : skill.id
-      }));
-    }
+  /**
+   * Maps the returned skills list to be used in the select skills input field
+   * select input field
+   * @param {Array} skills Array of object to be mapped
+   * @return {Object} list of skills
+   */
+  setSkills(skills) {
+    this.skills = skills.map(skill => ({
+      'label' : skill.name,
+      'value' : skill.id
+    }));
+  }
 
-    /**
-     * Validates the selected days input field
-     *
-     * @param {Object} selectedDays The form selectedDays array object to be validated
-     *
-     * @return {Object|Boolean} the validity of the field
-     */
-    validateSelectedDays(selectedDays: FormArray): any {
-      return selectedDays.value.indexOf(true) !== -1 ? null : {
-        selectedDays: {
-          valid: false
-        }
-      };
-    }
+  /**
+   * Validates the selected days input field
+   *
+   * @param {Object} selectedDays The form selectedDays array object to be validated
+   *
+   * @return {Object|Boolean} the validity of the field
+   */
+  validateSelectedDays(selectedDays: FormArray): any {
+    return selectedDays.value.indexOf(true) !== -1 ? null : {
+      selectedDays: {
+        valid: false
+      }
+    };
+  }
 
-    onSingleOpened() {
-      this.logSingle('- opened');
-    }
+  onSingleOpened() {
+    this.logSingle('- opened');
+  }
 
-    onSingleClosed() {
-      this.logSingle('- closed');
-    }
+  onSingleClosed() {
+    this.logSingle('- closed');
+  }
 
-    onSingleSelected(item) {
-      this.logSingle(`- selected (value: ${item.value}, label: ${item.label}`);
-    }
+  onSingleSelected(item) {
+    this.logSingle(`- selected (value: ${item.value}, label: ${item.label}`);
+  }
 
-    onSingleDeselected(item) {
-      this.logSingle(`- deselected (value: ${item.value}, label: ${item.label}`);
-    }
-    onMultipleOpened() {
-      this.logMultiple('- opened');
-    }
+  onSingleDeselected(item) {
+    this.logSingle(`- deselected (value: ${item.value}, label: ${item.label}`);
+  }
+  onMultipleOpened() {
+    this.logMultiple('- opened');
+  }
 
-    onMultipleClosed() {
-      this.logMultiple('- closed');
-    }
+  onMultipleClosed() {
+    this.logMultiple('- closed');
+  }
 
-    onMultipleSelected(item) {
-      this.logMultiple(`- selected (value: ${item.value}, label: ${item.label}`);
-    }
+  onMultipleSelected(item) {
+    this.logMultiple(`- selected (value: ${item.value}, label: ${item.label}`);
+  }
 
-    onMultipleDeselected(item) {
-      this.logMultiple(`- deselected (value: ${item.value}, label: ${item.label}`);
-    }
+  onMultipleDeselected(item) {
+    this.logMultiple(`- deselected (value: ${item.value}, label: ${item.label}`);
+  }
 
-    private logMultiple(msg: string) {
-      this.logMultipleString += `${msg} \n`;
-    }
+  private logMultiple(msg: string) {
+    this.logMultipleString += `${msg} \n`;
+  }
 
-    private logSingle(msg: string) {
-      this.logSingleString += `${msg} \n`;
-    }
+  private logSingle(msg: string) {
+    this.logSingleString += `${msg} \n`;
+  }
 
-    /**
-     * Takes the form values and makes POST request to the requests endpoint
-     * to create a new request.
-     * @param form
-     * @return {void}
-     */
-    requestMentor(form) {
-      form.value.selectedDays = form.value.selectedDays
-        .map((day, index) => day === true ? this.daysOfAvailability[index] : false)
-        .filter(day => day !== false);
-      const data = form.value;
-      return this.requestService.requestMentor(data)
-        .toPromise()
-        .then(() => this.snackBarOpen(true))
-        .catch(err => this.snackBarOpen(false));
+  /**
+   * Takes the form values and makes POST request to the requests endpoint
+   * to create a new request.
+   * @param form
+   * @return {void}
+   */
+  requestMentor(form) {
+    form.value.selectedDays = form.value.selectedDays
+      .map((day, index) => day === true ? this.daysOfAvailability[index] : false)
+      .filter(day => day !== false);
+    const data = form.value;
+    return this.requestService.requestMentor(data)
+      .toPromise()
+      .then(() => this.snackBarOpen(true))
+      .catch(err => this.snackBarOpen(false));
   }
 
   private snackBarOpen(status: Boolean) {
@@ -189,5 +189,4 @@ export class RequestsComponent implements OnInit {
         this.router.navigate(['/dashboard'], { queryParams: { refresh: 'dashboard'}});
       });
   }
-
 }
