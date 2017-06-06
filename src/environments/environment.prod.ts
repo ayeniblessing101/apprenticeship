d202c7c8c929c10d0049faf9b30b8510b083dfc9
andela-kakpobome <kesiena.akpobome@andela.com>
2017-06-06 03:57:34 +0300

    chore(env.staging): add a staging configuration file (#115)
    - modify the post install build script to read the appropriate ENV variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 import { environment } from '../env';
 
 environment.production = true;
-environment.apiBaseUrl = 'https://lenken-api-staging.herokuapp.com/api/v1';
+environment.apiBaseUrl = 'https://lenken-api.herokuapp.com/api/v1';
 environment.apiGateway = 'https://api.andela.com';
 environment.lenkenBaseUrl = 'https://lenken.andela.com';
 
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+import { environment } from '../env';
+
+environment.apiBaseUrl = 'https://lenken-api-staging.herokuapp.com/api/v1';
+environment.apiGateway = 'https://api-staging.andela.com';
+environment.lenkenBaseUrl = 'https://lenken-staging.andela.com';
+
+export { environment }
