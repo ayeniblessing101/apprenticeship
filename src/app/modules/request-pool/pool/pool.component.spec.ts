954ce324bd32b66d511d46ec38b18e81e6aea3cf
Solomon Kingsley <kingsley.solomon@andela.com>
2017-11-06 09:22:37 +0100

    chore(styles): fix styles on the request pool page (#248)
    Ensure the top navigation bar, left navigation and
    right sidebar is fixed on page scroll.
    
    [Delivers #152504159]

--- src/app/modules/request-pool/pool/pool.component.spec.ts
+++ src/app/modules/request-pool/pool/pool.component.spec.ts
@@ -1,14 +1,31 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+import { CommonModule } from '@angular/common';
+import { HttpModule, RequestOptions, XHRBackend, Response, ResponseOptions } from '@angular/http';
 import { PoolComponent } from './pool.component';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { RequestService } from './../../../services/request.service';
+import { RequestServiceStub } from '../../../stubs/request-stub.service';
+import { HttpService as Http } from '../../../services/http.service';
+import { HelperService } from '../../../services/helper.service';
 
 describe('PoolComponent', () => {
   let component: PoolComponent;
   let fixture: ComponentFixture<PoolComponent>;
+  let pageTitle: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PoolComponent ]
+      imports: [
+        InfiniteScrollModule,
+        HttpModule,
+      ],
+      declarations: [ PoolComponent ],
+      providers: [
+        Http,
+        { provide: RequestService, useClass: RequestServiceStub },
+        RequestService,
+        HelperService
+      ]
     })
     .compileComponents();
   }));
@@ -19,7 +36,8 @@ describe('PoolComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should be created', () => {
+  it('should create component instance', () => {
     expect(component).toBeTruthy();
   });
+
 });
