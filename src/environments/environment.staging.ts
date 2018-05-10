aa9f996d7084ad6d2d3b80d7cbf9a098c7ac4f44
q-ode <@Jungle1>
2018-05-10 16:51:08 +0300

    chore(environments): refactor api url to use gcp

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 import { environment } from '../env';
 
-environment.apiBaseUrl = 'https://lenken-api-staging.herokuapp.com/api';
+environment.apiBaseUrl = 'https://lenken-api-staging.andela.com/api';
 environment.apiGateway = 'https://api-staging.andela.com';
 environment.lenkenBaseUrl = 'https://lenken-v2.andela.com';
 environment.intercomAppId = 'dgixpcoe';
